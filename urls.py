e077124888312f685db175f90e6aea68860bd533
Andy Byers <andy.byers@openlibhums.org>
2018-09-24 14:30:31 +0100

    More work on base of this plugin.

--- urls.py
+++ urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     url(r'^admin/$', views.admin, name='unassigned_admin'),
     url(r'^$', views.index, name='unassigned_index'),
     url(r'^article/(?P<article_id>\d+)/$', views.unassigned_article, name='unassigned_article'),
+    url(r'^article/(?P<article_id>\d+)/assign/(?P<editor_id>\d+)/type/(?P<assignment_type>[-\w.]+)/$',
+        views.assign_editor, name='unassigned_assign_editor'),
+    url(r'^article/(?P<article_id>\d+)/unassign/(?P<editor_id>\d+)/$', views.unassign_editor,
+        name='unassigned_unassign_editor'),
+    url(r'^article/(?P<article_id>\d+)/notify/(?P<editor_id>\d+)/$', views.assignment_notification,
+        name='unassigned_assignment_notification'),
 ]
--- views.py
+++ views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.urlresolvers import reverse
+from django.contrib import messages
+from django.db import IntegrityError
 
 from submission import models
-from security.decorators import editor_user_required
+from security.decorators import editor_user_required, senior_editor_user_required
 from utils import ithenticate
 from core import models as core_models
 from review import models as review_models
+from review.logic import get_assignment_content
+from events import logic as event_logic
+from utils import models as util_models
 
 
 @editor_user_required
@@ -65,6 +70,109 @@ def unassigned_article(request, article_id):
         'article': article,
         'editors': editors,
         'section_editors': section_editors,
+    }
+
+    return render(request, template, context)
+
+
+@senior_editor_user_required
+def assign_editor(request, article_id, editor_id, assignment_type, should_redirect=True):
+    """
+    Allows a Senior Editor to assign another editor to an article.
+    :param request: HttpRequest object
+    :param article_id: Article PK
+    :param editor_id: Account PK
+    :param assignment_type: string, 'section-editor' or 'editor'
+    :param should_redirect: if true, we redirect the user to the notification page
+    :return: HttpResponse or HttpRedirect
+    """
+    article = get_object_or_404(models.Article, pk=article_id)
+    editor = get_object_or_404(core_models.Account, pk=editor_id)
+
+    if not editor.has_an_editor_role(request):
+        messages.add_message(request, messages.WARNING, 'User is not an Editor or Section Editor')
+        return redirect(reverse('unassigned_article', kwargs={'article_id': article.pk}))
+
+    try:
+        assignment = review_models.EditorAssignment.objects.create(article=article, editor=editor, editor_type=assignment_type)
+        messages.add_message(request, messages.SUCCESS, '{0} added as an Editor'.format(editor.full_name()))
+
+        kwargs = {'user_message_content': '',
+                  'editor_assignment': assignment,
+                  'request': request,
+                  'skip': True,
+                  'acknowledgement': False}
+
+        event_logic.Events.raise_event(event_logic.Events.ON_ARTICLE_ASSIGNED, task_object=article, **kwargs)
+
+        if should_redirect:
+            return redirect('{0}?return={1}'.format(
+                reverse('unassigned_assignment_notification', kwargs={'article_id': article_id, 'editor_id': editor.pk}),
+                request.GET.get('return')))
+    except IntegrityError:
+        messages.add_message(request, messages.WARNING,
+                             '{0} is already an Editor on this article.'.format(editor.full_name()))
+        if should_redirect:
+            return redirect(reverse('unassigned_article', kwargs={'article_id': article_id}))
+
+
+@senior_editor_user_required
+def unassign_editor(request, article_id, editor_id):
+    """Unassigns an editor from an article"""
+    article = get_object_or_404(models.Article, pk=article_id)
+    editor = get_object_or_404(core_models.Account, pk=editor_id)
+    assignment = get_object_or_404(review_models.EditorAssignment, article=article, editor=editor)
+
+    assignment.delete()
+
+    util_models.LogEntry.add_entry(types='EditorialAction',
+                                   description='{0} unassigned from article {1}'.format(editor.full_name(), article.id),
+                                   level='Info',
+                                   request=request, target=article)
+
+    return redirect(reverse('unassigned_article', kwargs={'article_id': article_id}))
+
+
+@senior_editor_user_required
+def assignment_notification(request, article_id, editor_id):
+    """
+    A senior editor can sent a notification to an assigned editor.
+    :param request: HttpRequest object
+    :param article_id: Article PK
+    :param editor_id: Account PK
+    :return: HttpResponse or HttpRedirect
+    """
+    article = get_object_or_404(models.Article, pk=article_id)
+    editor = get_object_or_404(core_models.Account, pk=editor_id)
+    assignment = get_object_or_404(review_models.EditorAssignment, article=article, editor=editor, notified=False)
+
+    email_content = get_assignment_content(request, article, editor, assignment)
+
+    if request.POST:
+
+        email_content = request.POST.get('content_email')
+        kwargs = {'user_message_content': email_content,
+                  'editor_assignment': assignment,
+                  'request': request,
+                  'skip': False,
+                  'acknowledgement': True}
+
+        if 'skip' in request.POST:
+            kwargs['skip'] = True
+
+        event_logic.Events.raise_event(event_logic.Events.ON_ARTICLE_ASSIGNED_ACKNOWLEDGE, **kwargs)
+
+        if request.GET.get('return', None):
+            return redirect(request.GET.get('return'))
+        else:
+            return redirect(reverse('unassigned_article', kwargs={'article_id': article_id}))
+
+    template = 'unassigned/assignment_notification.html'
+    context = {
+        'article': article_id,
+        'editor': editor,
+        'assignment': assignment,
+        'email_content': email_content,
     }
 
     return render(request, template, context)
